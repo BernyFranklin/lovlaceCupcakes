e863c7cb4b7e3de0676cbae767d685897b5a49bf
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-09-08 13:19:42 -0700

    Shell created

---
+++ src/lovelacecupcakes/src/main/java/com/cis055/App.java
@@ -0,0 +1,19 @@
+/*
+ * lovelaceCupcakes 
+ * Frank Bernal
+ * CIS-055
+ * Date Started: 08SEP2022
+ * Date Finished: 
+ */
+
+package com.cis055;
+
+// Start App
+public class App 
+{
+    // Start main
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
